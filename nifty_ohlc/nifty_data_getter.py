9f8cc53f6e1cbb8bb6ef1d371c410876c1cb9ed3
ManavButani <butanimanav@gmail.com>
2024-09-21 22:11:49 +0530

    He prabhu madad karaje

--- nifty_ohlc/nifty_data_getter.py
+++ nifty_ohlc/nifty_data_getter.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 ticker_symbol = '^NSEI'
 start_date =  '2024-09-01'
-end_date = '2024-09-17'
+end_date = '2024-09-21'
 
-df = yf.download(ticker_symbol, start=start_date, end=end_date, interval='5m')
+df = yf.download(ticker_symbol, start=start_date, end=end_date, interval='1d')
 df.index.name = df.index.name.lower()
 df.columns = df.columns.str.lower()
 
@@ -23,10 +23,12 @@ df['max'] = df[['open', 'high', 'low', 'close']].max(axis=1)
 df['min'] = df[['open', 'high', 'low', 'close']].min(axis=1)
 df['difference'] = df['max'] - df['min']
 df['candle_body'] = df['open'] - df['close']
-df['high_shadow'] = df['high'] - df['open']
-df['low_shadow'] = df['close'] - df['low']
+df['high_shadow'] = df['high'] - df[['open', 'close']].max(axis=1)
+df['low_shadow'] = df['low'] - df[['open', 'close']].min(axis=1)
+df['low-open'] = df['low'] - df['open']
+df['high-open'] = df['high'] - df['open']
 df['trend'] = df['candle_body'].apply(lambda x: "DOWN" if x < 0 else "ZERO" if x == 0 else "UP")
-df.to_csv('./nifty_ohlc/nifty_5m_september_2024.csv', index=True)
+df.to_csv('./nifty_ohlc/nifty_1d_september_2024.csv', index=True)
 
 
 # df = yf.download(ticker_symbol, start=start_date, end=end_date, interval='5m')
