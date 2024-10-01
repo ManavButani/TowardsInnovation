e6a1af1ec859a04454cb90aefbdb1114e41b12e6
ManavButani <butanimanav@gmail.com>
2024-10-01 19:45:40 +0530

    audio joiner

--- video_urls.py
+++ video_urls.py
@@ -1,7 +1,7 @@
 from pytube import Playlist
 
 # Enter your YouTube playlist URL here
-playlist_url = "https://youtube.com/playlist?list=PL_x_hd_0x_CL7qGXbKSbuqTfYVsK8Ojf3&si=jLl_L1f61u7j5WAt"
+playlist_url = "https://youtube.com/playlist?list=PLxNHpNhDaEFLTT826yucpRLr6TAMSKziU&si=bV1zJRY7nPANGm79"
 
 # Create Playlist object
 playlist = Playlist(playlist_url)
