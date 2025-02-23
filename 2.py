8f331515b2d46da383227127605bd29be79ca2e2
ManavButani <manavkrenil@gmail.com>
2025-02-23 13:33:10 +0530

    successfully does undo

--- 2.py
+++ 2.py
@@ -1,11 +1,7 @@
 import pandas as pd
-df = pd.read_csv('merged_data.csv')
-
-yesterday_open = df["Open"].shift(1)
-df["open %change"] = (yesterday_open - df["Open"]) / yesterday_open * 100
-
+df = pd.read_csv('merged_data.csv') 
 df = df[~df["Type"].isin(["CE", "PE"])]
 df["Trade"] = df["P/L"].apply(lambda x: 0 if x < 700 else 1)
-df = df[['Close', '% Change', 'Trade', "open %change", "Open"]]
+df = df[['Close', '% Change', 'Trade']]
 
 df.to_csv('data_class.csv')
